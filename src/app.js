9e110040fd1c6efa1aed9e37f545b9b46a54d0bf
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-12 08:33:30 -0300

    agregue nodemon

--- src/app.js
+++ src/app.js
@@ -57,45 +57,6 @@ app.use('/api/ingredientesXpizzas', routerIngXPizza);
 app.use('/api/unidades', routerUnidad);
 app.use('/api/ingredientes', routerIngrediente);
 
-/* let svc = new PizzaService()
-
-app.get('/getall', (req, res) => {
-   let listaPizzas = svc.getAll()
-   listaPizzas.then((resp) => { res.send(resp) })
-})
-
-app.get('/getbyid/:id', (req, res) => {
-   let respuesta = svc.getById(req.params.id)
-   respuesta.then((resp) => { res.send(resp) })
-})
-
-app.post('/insert', (req, res) => {
-   let pizzaNueva = new Pizza();
-   pizzaNueva.Nombre = req.body.Nombre
-   pizzaNueva.LibreGluten = req.body.LibreGluten
-   pizzaNueva.Importe = req.body.Importe
-   pizzaNueva.Descripcion = req.body.Descripcion
-   console.log(pizzaNueva);
-   let respuesta = svc.insert(pizzaNueva)
-   respuesta.then((resp) => { res.send("Pizza creada")})
-})
-
-app.put('/update', (req, res) => {
-   let pizzaNueva = new Pizza();
-   pizzaNueva.Id = req.body.Id;
-   pizzaNueva.Nombre = req.body.Nombre;
-   pizzaNueva.LibreGluten = req.body.LibreGluten;
-   pizzaNueva.Importe = req.body.Importe;
-   pizzaNueva.Descripcion = req.body.Descripcion;
-   let respuesta = svc.update(pizzaNueva)
-   respuesta.then((resp) => { res.send("Pizza actualizada")})
-})
-
-app.delete('/delete/:id', (req, res) => {
-   let respuesta = svc.deleteById(req.params.id)
-   respuesta.then((resp) => { res.send("Pizza eliminada") })
-})  */
-
 app.listen(3000, () => {
     console.log("Servidor a la espera de conexiones")
 })  
